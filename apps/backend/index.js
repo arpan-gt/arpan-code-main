f5ca62cda7a512ac6a5d6f16e417404fda329430
Arpan Kumar <arpan.main@outlook.com>
2025-11-10 23:59:45 +0530

    updated cors

--- apps/backend/index.js
+++ apps/backend/index.js
@@ -9,11 +9,7 @@ import userRouter from "./routes/user.routes.js";
 
 const app = express();
 app.use(
-  cors({
-    origin: [process.env.CLIENT_URL, "http://localhost:5173"],
-
-    credentials: true,
-  })
+  cors()
 );
 
 app.options("*", cors());
